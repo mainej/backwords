9bb7d5cd70b66b23cae60559bc220e7dfc113fd1
Jacob Maine <jacob.maine@gmail.com>
2020-03-23 18:01:16 -0700

    Fix errors during CSS prod build

--- postcss.config.js
+++ postcss.config.js
@@ -13,9 +13,16 @@ module.exports = {
     plugins: [
         require('postcss-import'),
         require('tailwindcss'),
-        require('postcss-preset-env')({ stage: 1 }),
-        ...process.env.NODE_ENV === 'production'
-            ? [purgecss]
-            : []
+        require('postcss-preset-env')({
+            stage: 1,
+            // Temporarily disabled, until
+            // https://github.com/jonathantneal/postcss-focus-within/pull/5 is
+            // released. See
+            // https://github.com/tailwindcss/tailwindcss/issues/1190
+            features: {
+                'focus-within-pseudo-class': false
+            }
+        }),
+        ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
     ]
 };
